8502905230b2b385a4f16418a17521bc01baf26a
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-06-04 21:55:48 +1200

    Add the environment variables for the resource version

--- src/cookbooks/base_windows/recipes/meta.rb
+++ src/cookbooks/base_windows/recipes/meta.rb
@@ -12,6 +12,26 @@ env 'BASE_IMAGE' do
   value resource_name
 end
 
+resource_version_major = node['resource']['version_major']
+env 'RESOURCE_VERSION_MAJOR' do
+  value resource_version_major
+end
+
+resource_version_minor = node['resource']['version_minor']
+env 'RESOURCE_VERSION_MINOR' do
+  value resource_version_minor
+end
+
+resource_version_patch = node['resource']['version_patch']
+env 'RESOURCE_VERSION_PATCH' do
+  value resource_version_patch
+end
+
+resource_version_semantic = node['resource']['version_semantic']
+env 'RESOURCE_VERSION_SEMANTIC' do
+  value resource_version_semantic
+end
+
 env 'STATSD_ENABLED_SERVICES' do
   value 'consul'
 end
--- src/cookbooks/base_windows/spec/meta_spec.rb
+++ src/cookbooks/base_windows/spec/meta_spec.rb
@@ -9,5 +9,25 @@ describe 'base_windows::meta' do
     it 'sets the BASE_IMAGE environment variable' do
       expect(chef_run).to create_env('BASE_IMAGE')
     end
+
+    it 'sets the RESOURCE_VERSION_MAJOR environment variable' do
+      expect(chef_run).to create_env('RESOURCE_VERSION_MAJOR')
+    end
+
+    it 'sets the RESOURCE_VERSION_MINOR environment variable' do
+      expect(chef_run).to create_env('RESOURCE_VERSION_MINOR')
+    end
+
+    it 'sets the RESOURCE_VERSION_PATCH environment variable' do
+      expect(chef_run).to create_env('RESOURCE_VERSION_PATCH')
+    end
+
+    it 'sets the RESOURCE_VERSION_SEMANTIC environment variable' do
+      expect(chef_run).to create_env('RESOURCE_VERSION_SEMANTIC')
+    end
+
+    it 'sets the STATSD_ENABLED_SERVICES environment variable' do
+      expect(chef_run).to create_env('STATSD_ENABLED_SERVICES')
+    end
   end
 end
